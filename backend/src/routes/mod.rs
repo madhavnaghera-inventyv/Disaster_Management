f855fc5581c75b8671953cdf47019e4afaaa0de7
lakshya.suthar.inventyv <L@kshya12102003>
2025-02-21 17:36:43 +0530

    added cors

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -1,24 +1,30 @@
 use std::sync::Arc;
 
 use axum::middleware::from_fn;
-use axum::routing::get;
 use axum::{Extension, Router};
+use tower::ServiceBuilder;
 use crate::{shelters, user, disaster};
 use crate::utils::db::AppState;
 use crate::middleware::log::log_request;
 use crate::resources;
+use tower_http::cors::{CorsLayer, Any};
+use http::{Request, Response, Method, header};
 
 pub fn merge_routes(state: Arc<AppState>) -> Router  {
+    let cors = CorsLayer::new()
+    // allow GET and POST when accessing the resource
+    .allow_methods([Method::GET, Method::POST])
+    // allow requests from any origin
+    .allow_origin(Any);
+
+
     Router::new()
-        .route("/test", get(test_handler))
+
         .nest("/shelters", shelters::shelters_routes(state.clone())) 
         .nest("/user", user::user_routes(state.clone()))
         .nest("/resources", resources::resources_routes((*state).clone()))
         .nest("/disaster", disaster::create_routes(state.clone())) 
         .layer(Extension(state.clone())) 
         .layer(from_fn(log_request))
-}
-
-async fn test_handler() -> &'static str {
-    "test response"
+        .layer(ServiceBuilder::new().layer(cors))
 }
