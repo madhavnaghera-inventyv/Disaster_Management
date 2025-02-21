1dfb81982371ca28e113acac8db3ab4d22562045
lakshya.suthar.inventyv <L@kshya12102003>
2025-02-21 15:39:04 +0530

    made test api

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use axum::middleware::from_fn;
+use axum::routing::get;
 use axum::{Extension, Router};
 use crate::{shelters, user, disaster};
 use crate::utils::db::AppState;
@@ -9,11 +10,15 @@ use crate::resources;
 
 pub fn merge_routes(state: Arc<AppState>) -> Router  {
     Router::new()
-
+        .route("/test", get(test_handler))
         .nest("/shelters", shelters::shelters_routes(state.clone())) 
         .nest("/user", user::user_routes(state.clone()))
         .nest("/resources", resources::resources_routes((*state).clone()))
         .nest("/disaster", disaster::create_routes(state.clone())) 
         .layer(Extension(state.clone())) 
         .layer(from_fn(log_request))
+}
+
+async fn test_handler() -> &'static str {
+    "test response"
 }
